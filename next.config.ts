b2dbbe1249b530b34f97b13094cba47395334562
adimanab <manab.das@cbnits.com>
2026-02-03 18:00:29 +0530

    home page added

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,6 @@
-import type { NextConfig } from "next";
-
 /** @type {import('next').NextConfig} */
-const nextConfig: NextConfig = {
-  webpack: (config, { dev, isServer }) => {
-    // Enable polling for Docker environments
-    if (dev && !isServer) {
-      config.watchOptions = {
-        poll: 800,       // Check for changes every 800ms
-        aggregateTimeout: 300,
-      };
-    }
-    return config;
-  },
-};
+const nextConfig = {
+  turbopack: {}, // 👈 silence the error
+}
 
-export default nextConfig;
+module.exports = nextConfig;
